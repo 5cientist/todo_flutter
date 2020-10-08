9b7495332f436bf3b7a3a55e242419179f0b4640
hanshir <hanshirlukku@gmail.com>
2020-10-08 11:41:33 +0530

    added scafold

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp();
   }
 }
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
